272f0395d3501569c30cd0f202932452683bb3ac
dombonifacio <nikkibonifacio184@gmail.com>
2024-01-11 21:51:43 -0800

    Fixed Palindrom Problem.

--- 9-IsPalindrome.py
+++ 9-IsPalindrome.py
@@ -2,7 +2,9 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         # Use two-pointer method:
         
-        arr = list(str(x))
+        if x < 0:
+            return False
+        arr = str(x)
         l, r = 0, len(arr) - 1
         
         # While the left pointer is not greater than the right pointer and both pointers hit the same index and none of the cases failed, then it is a palindrome
@@ -11,15 +13,16 @@ class Solution:
             if arr[l] != arr[r]:
                 print("it is not a palindrome")
                 return False
-            print("it is a palindrome")
-             # If l and r pointer matches with each other in every loop, it is a palindrome
-            return True
+            l+=1
+            r-=1
+        return True
+           
 
         
 
 def main():
     sol = Solution()
-    sol.isPalindrome(293)
+    sol.isPalindrome(121)
         
 if __name__ == "__main__":
     main()
